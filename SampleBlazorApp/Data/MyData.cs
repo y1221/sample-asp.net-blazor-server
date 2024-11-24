ce6fbb12dfa0fc00d8b4c570717f809382faf3c0
okayu <yuto.gio.0515@gmail.com>
2024-11-24 10:22:27 +0900

    モデルクラスとフォームの利用
    ■EditForm
    Model：バインドするモデルインスタンスを指定
    OnValidSubmit：モデルに定義した検証を行い、問題ない場合のみ処理を指定した処理を実行
      DataAnnotationsValidator：検証の実行
      ValidationSummary：検証結果の表示

---
+++ SampleBlazorApp/Data/MyData.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleBlazorApp.Data
+{
+    public class MyData
+    {
+        [Required]
+        public string Name { get; set; } = default!;
+
+        public int Age { get; set; }
+
+        [EmailAddress]
+        public string Mail { get; set; } = default!;
+
+        public override string ToString()
+        {
+            return $"[{Name} ({Age}) {Mail}]";
+        }
+    }
+}
